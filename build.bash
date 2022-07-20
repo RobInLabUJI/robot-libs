c6632aa6f3febb78424f52b71278b559abbb3630
ecervera <Enric Cervera>
2022-07-20 16:39:06 +0200

    Build with CUDA

--- build.bash
+++ build.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build -t robinlab/robot-libs:20.04 .
+docker build -t robinlab/robot-libs:20.04 -f Dockerfile .
 
 docker build -t robinlab/robot-libs-ros:20.04 -f Dockerfile-ros .
+
+docker build -t robinlab/robot-libs-ros-ml:20.04 -f Dockerfile-ros-ml .
